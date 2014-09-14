bea35ae70abc206f90e47c8c2072d60c2b1f1fe6
Edson Samuel Jr <samuedson@gmail.com>
2014-09-14 10:40:38 -0300

    adding zepto

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-$ = function(id) { return document.getElementById(id); };
-
-$('urls').oninput = function() {
-    document.getElementsByTagName('footer')[0].textContent = $('urls').value;
-};
+$('#urls').on('input', function() {
+	$('footer')[0].textContent = $('#urls').val();
+});
