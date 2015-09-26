d23aa6975ca31a05dea0b6d0e73c1b440939146b
Edson Samuel Jr <samuedson@gmail.com>
2015-09-26 19:01:14 -0300

    fix fetching file name by image url

--- baixagram.rb
+++ baixagram.rb
@@ -4,19 +4,20 @@ require 'open-uri'
 # baixagram
 
 begin
-    ARGV.each do|insta_url|
-        insta = Nokogiri::HTML(open(insta_url))
-        # instapp:owner_user_id
-        img_url = insta.css('meta[property="og:image"]').attr('content').value
-        fname = img_url[49..img_url.length]
-        puts "\nCarregando foto da URL: #{img_url}"
-        open(img_url) { |fi|
-            File.open(fname, "wb") do |file|
-                file.puts fi.read
-            end
-        }
-        puts "\n...foto #{fname} baixada com sucesso"
+  ARGV.each do|insta_url|
+    insta = Nokogiri::HTML(open(insta_url))
+    # instapp:owner_user_id
+    img_url = insta.css('meta[property="og:image"]').attr('content').value
+    fname = /([^\/]+$)/.match(img_url).to_s
+    puts "\nCarregando foto da URL: #{img_url}"
+    open(img_url) do |img|
+      File.open(fname, 'w') do |file|
+        file.puts img.read
+        file.close
+      end
     end
+    puts "\n...foto #{fname} baixada com sucesso"
+  end
 rescue
-   puts "\nAlgo muito estranho tá acontecendo aqui... fale com o doido do lado" 
+  puts "\nAlgo muito estranho tá acontecendo aqui... fale com o doido do lado"
 end
