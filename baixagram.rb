a4c81d9e822c7f60a8e8a889b6ce0e39efdfa810
Edson Samuel Jr <samuedson@gmail.com>
2015-09-26 19:11:33 -0300

    remove comment

--- baixagram.rb
+++ baixagram.rb
@@ -5,25 +5,3 @@ require 'open-uri'
 get '/' do
     erb :index
 end
-
-# baixagram
-=begin
-begin
-  ARGV.each do|insta_url|
-    insta = Nokogiri::HTML(open(insta_url))
-    # instapp:owner_user_id
-    img_url = insta.css('meta[property="og:image"]').attr('content').value
-    fname = /([^\/]+$)/.match(img_url).to_s
-    puts "\nCarregando foto da URL: #{img_url}"
-    open(img_url) do |img|
-      File.open(fname, 'w') do |file|
-        file.puts img.read
-        file.close
-      end
-    end
-    puts "\n...foto #{fname} baixada com sucesso"
-  end
-rescue
-  puts "\nAlgo muito estranho tá acontecendo aqui... fale com o doido do lado"
-end
-=end
