eebd6c83cc2c4f1bf028bd7bc8ffd7c40534b95a
Edson Samuel Jr <samuedson@gmail.com>
2016-02-28 16:16:21 -0300

    fix some styles again; update zepto dep

--- public/script.js
+++ public/script.js
@@ -1,5 +1,12 @@
-(function(){
-	$('#urls').on('input', function() {
-		console.log(this.value);
-	});
+(function() {
+	var $urls = $('#urls'),
+		$list = $('section .list'),
+		instamatic = function(value) {
+			console.log(value);
+		}
+		onInputChange = function() {
+			instamatic(this.value);
+		};
+
+	$urls.on('input', onInputChange);
 })();
