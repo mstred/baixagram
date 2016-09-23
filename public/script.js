da2920c5631af579748b71fcbb2e27c73aa71106
Edson Samuel Jr <samuedson@gmail.com>
2016-09-23 20:51:14 -0300

    fix image placement after response; add some chars to url regex

--- public/script.js
+++ public/script.js
@@ -1,18 +1,20 @@
 (function() {
 	var $urls = $('#urls'),
 		$list = $('section .list'),
-		urlRegEx = /http(s)?\:\/\/(www.)?instagram\.com\/p\/[A-Za-z0-9]+/gim;
+		urlRegEx = /http(s)?\:\/\/(www.)?instagram\.com\/p\/[A-Za-z0-9_-]+/gim;
 
 	$urls.on('blur', function() {
 		var value = this.value;
 		var urls = (typeof value === 'string' && value.match(urlRegEx) || []);
 
+		$list.empty();
+
 		if (urls && urls.length > 0) {
 			$.getJSON('/images', {urls: urls}, function(response) {
 				if (response && response.length > 0) {
-					$.each(response, function(i, o) {
-						$list.append($('img').attr('src', o));
-					});
+					$list.append($.map(response, function(o) {
+						return $('<img>').attr({src: o, class: 'block instant'});
+					}));
 				}
 			});
 		}
